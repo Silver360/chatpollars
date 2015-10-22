43e33685f855117400055977d38e60c7b3ca940e
ldraminski <lukasz.daminski@agora.pl>
2015-10-22 07:35:13 +0200

    Update Services

---
+++ app/public/js/services/serviceMessages.js
@@ -0,0 +1,38 @@
+
+var app = angular.module('dollars');
+
+app.service('ServiceMessages', ['socketio', '$rootScope', function( socketio, $rootScope ){
+
+    var messages  = {};
+
+    socketio.on('new:message', function(data){
+            messages[data.id] = data;
+            $rootScope.$broadcast("new:message");
+    });
+
+    socketio.on('new:messages', function(data){
+            messages = data;
+            $rootScope.$broadcast("new:message");
+    });
+
+    this.sendMessage = function (message){
+        socketio.emit('send:message', message);
+    };
+
+    this.getMessage = function() {
+        return messages;
+    }
+
+    this.getMessages = function() {
+        socketio.emit('get:messages');
+    }
+
+    Object.size = function(obj) {
+        var size = 0, key;
+        for (key in obj) {
+            if (obj.hasOwnProperty(key)) size++;
+        }
+        return size;
+    };
+
+}]);
