ec80c16671c84b2b62a76a0ed4d578a5d72a96fc
ldraminski <lukasz.daminski@agora.pl>
2015-11-19 11:33:12 +0100

    Init

--- app/bin/routes.js
+++ app/bin/routes.js
@@ -4,95 +4,59 @@ var bodyParser = require('body-parser');
 
 module.exports = {
 
-    app: {},
     express: require('express'),
-    io: {},
     userReq: require('./controllers/users_controller.js'),
     msgReq: require('./controllers/messages_controller.js'),
     sessionReq: require('./controllers/session_controller.js'),
     usersSocket: {},
     init: function(app, io){
-        this.app = app;
-        this.io = io;
 
-        this.static(this.app);
-        this.login(this.io, this.userReq, this.sessionReq);
-        this.logout(this.app, this.sessionReq);
-        this.message(this.io, this.msgReq, this.sessionReq);
-        this.messages(this.io, this.msgReq);
-        this.authentication(this.app, this.sessionReq, this.io);
-        this.findSession(this.app, this.io);
-        this.requestHandler(this.app);
-        this.signin(this.io, this.userReq, this.sessionReq);
+        this.static(app);
+        this.requestHandler(app, io, this.userReq, this.msgReq, this.sessionReq);
     },
     static: function(app){
         app.use( '/', this.express.static('./public') );
     },
-    findSession: function(app, io){
-        app.get( '/find', function(req, res){
-            res.send(req.session);
-        });
-
-        io.sockets.on('connection', function(socket){
-            socket.on('find', function(data){
-                io.sockets.emit('done', socket.request.session);
-            });
+    findSession: function(req, res){
+        res.send(req.session);
+    },
+    login: function(data, io, userReq, sessionReq){
+        userReq.login(data.login, data.password).then(function(data){
+            sessionReq.createSession(socket.request, data);
+            io.sockets.emit('login:res', 'access');
+        }).catch(function(e){
+            io.sockets.emit('login:res', '' + e);
         });
     },
-    login: function(io, userReq, sessionReq){
-        io.sockets.on('connection', function(socket){
-            socket.on('login', function(data){
-                userReq.login(data.login, data.password).then(function(data){
-                    sessionReq.createSession(socket.request, data);
-                    io.sockets.emit('login:res', 'access');
-                }).catch(function(e){
-                    io.sockets.emit('login:res', '' + e);
-                });
-            });
+    signin: function(data, io, userReq, sessionReq){
+        userReq.signin(data.login, data.password).then(function(data){
+            sessionReq.createSession(socket.request, data);
+            io.sockets.emit('login:res', 'access');
+        }).catch(function(e){
+            io.sockets.emit('login:res', '' + e);
         });
     },
-    signin: function(io, userReq, sessionReq){
-        io.sockets.on('connection', function(socket){
-            socket.on('signin', function(data){
-                userReq.signin(data.login, data.password).then(function(data){
-                    sessionReq.createSession(socket.request, data);
-                    io.sockets.emit('login:res', 'access');
-                }).catch(function(e){
-                    io.sockets.emit('login:res', '' + e);
-                });
-            });
-        });
-    },
-    logout: function(app, sessionReq){
-        app.get( '/logout', function(req, res){
-            console.log('Zosta�es wylogowany');
-            sessionReq.destroySesssion(req, function(){
-                res.redirect('/');
-            });
-
+    logout: function(req, res, sessionReq){
+        console.log('Zosta�es wylogowany');
+        sessionReq.destroySesssion(req, function(){
+            res.redirect('/');
         });
     },
     banAccount: function(io, userReq, sessionReq){
-        io.sockets.on('connection', function(socket){
-            socket.on('ban:perma', function(data){
-                
-            });
-        });
+
     },
-    authentication: function(app, sessionReq, io){
-        app.post( '/authentication', function(req, res){
-            if (sessionReq.verificationSession(req) === 'access') {
-                console.log('Wszystko ok [REST]');
-                res.send({ res: 'access:go', user: { login: req.session.user.login, group: req.session.user.group } });
+    authentication: function(req, res, sessionReq){
+        if (sessionReq.verificationSession(req) === 'access') {
+            console.log('Wszystko ok [REST]');
+            res.send({ res: 'access:go', user: { login: req.session.user.login, group: req.session.user.group } });
+        } else {
+            if(req.body.url == '/login' || req.body.url == '/prelogin') {
+                console.log('Sesja nie jest aktywna ale mozesz tu byc :) [REST]');
             } else {
-                if(req.body.url == '/login' || req.body.url == '/prelogin') {
-                    console.log('Sesja nie jest aktywna ale mozesz tu byc :) [REST]');
-                } else {
-                    console.log('Jestes w niew��sciwym miejscu [REST]');
-                    res.send({ res: 'access:denied', user: null});
-                }
+                console.log('Jestes w niew��sciwym miejscu [REST]');
+                res.send({ res: 'access:denied', user: null});
             }
-        });
+        }
 
         // Wersja pod Sockety
 
@@ -112,34 +76,72 @@ module.exports = {
         //     });
         // });
     },
-    messages: function(io, msgReq){
-      io.sockets.on('connection', function(socket){
-          socket.on('get:messages', function(data){
-              io.sockets.emit('new:messages', msgReq.getMessages());
-          });
+    messages: {
+        getMessages: function(data, io, msgReq){
+            io.sockets.emit('new:messages', msgReq.getMessages());
+        },
+        deleteMessage: function(data, io, msgReq, socket, callback){
+            console.log(data);
+            msgReq.deleteMessage(data, socket.request);
+            io.sockets.emit('new:messages', msgReq.getMessages());
+        },
+        sendMessage: function(data, io, msgReq, socket){
+            if(data === '/logout'){
+                sessionReq.destroySesssion(socket.request, function(){
+                    console.log('Wylogowany');
+                    io.sockets.emit('access:denied', false);
+                });
+            } else {
+                io.sockets.emit('new:message', msgReq.createMsg(data, socket.request));
+            }
+        }
+    },
+    requestHandler: function(app, io, userReq, msgReq, sessionReq){
 
-          socket.on('delete:messages', function(data, callback){
-              console.log(data);
-              msgReq.deleteMessage(data, socket.request);
-              io.sockets.emit('new:messages', msgReq.getMessages());
-          });
-      });
-    },
-    message: function(io, msgReq, sessionReq){
-        io.sockets.on('connection', function(socket){
-            socket.on('send:message', function(data){
-                if(data === '/logout'){
-                    sessionReq.destroySesssion(socket.request, function(){
-                        console.log('Wylogowany');
-                        io.sockets.emit('access:denied', false);
-                    });
-                } else {
-                    io.sockets.emit('new:message', msgReq.createMsg(data, socket.request));
-                }
-            });
+        app.get( '/find', function(req, res){
+            module.exports.findSession(req, res);
         });
-    },
-    requestHandler: function(app){
+
+        app.get( '/logout', function(req, res){
+            module.exports.logout(req, res, sessionReq);
+        });
+
+        app.post( '/authentication', function(req, res){
+            module.exports.authentication(req, res, sessionReq, io);
+        });
+
+        io.sockets.on('connection', function(socket){
+            socket.on('find', function(data){
+                io.sockets.emit('done', socket.request.session);
+            });
+
+            socket.on('login', function(data){
+                module.exports.login(data, io, userReq, sessionReq);
+            });
+
+            socket.on('signin', function(data){
+                module.exports.signin(data, io, userReq, sessionReq);
+            });
+
+            socket.on('ban:perma', function(data){
+                module.exports.banAccount(data, io, userReq, sessionReq);
+            });
+
+            socket.on('get:messages', function(data){
+                module.exports.messages.getMessages(data, io, msgReq);
+            });
+
+            socket.on('delete:messages', function(data, callback){
+                module.exports.messages.deleteMessage(data, io, msgReq, socket, callback);
+            });
+
+            socket.on('send:message', function(data){
+                module.exports.messages.sendMessage(data, io, msgReq, socket);
+            });
+
+
+        });
+
         app.get( '/chat', function(req, res){
             res.redirect('/#/chat');
         });
